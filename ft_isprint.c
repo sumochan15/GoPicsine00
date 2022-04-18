f2960d0ce9bd9defa15c209b6367a1089b019b57
ymorimot <dorariver123@gmail.com>
2022-04-19 04:04:07 +0900

    4190400

--- ft_isprint.c
+++
@@ -1,4 +0,0 @@
-int ft_isprint(int c)
-{
-    return(32 <= c && c <= 126);
-}
--- libft.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBFT_H
-#define LIBFT_H
-
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-
-#endif
