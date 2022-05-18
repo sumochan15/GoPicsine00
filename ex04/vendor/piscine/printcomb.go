051d9a7c1909e7f4192b2722cffc9419e46a47a1
ymorimot <dorariver123@gmail.com>
2022-05-19 07:07:10 +0900

    20220518

--- ex04/vendor/piscine/printcomb.go
+++ ex04/vendor/piscine/printcomb.go
@@ -3,7 +3,7 @@ package piscine
 import "ft"
 
 func PrintComb(){
-	for i :=0; i <=7; i++{
+	for i := 0; i <= 7; i++{
 		for j := i + 1; j <= 8; j++{
 			for k := j + 1; k <= 9; k++{
 				ft.PrintRune(rune(i + '0'))
